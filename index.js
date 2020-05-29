3022bf377dd25273d10f97c03076ec773d467f9c
Janatbek Sharsheyev <janatbek@gmail.com>
2020-05-28 21:46:03 -0600

    add fake db and api

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const path = require('path');
+const guid = require('guid');
+
 
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('hello from live server')
+	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(80, console.log('listening'));
+app.get('/api/employees', (req, res) => {
+	const employees = {
+		id: guid.create(),
+		name: 'Janatbek',
+		lastName: 'Orozaly',
+		email: 'janatbek@gmail.com',
+		city: 'Boise',
+		state: 'Idaho'
+	};
+	res.json(employees);
+});
+
+app.listen(3000, console.log('listening'));
 
